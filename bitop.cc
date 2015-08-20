593f9f4efac2be917401b723dcbfdd2054d12c7f
gogdizzy <gogdizzy@gmail.com>
2015-08-20 23:34:28 +0800

    add bitop.cc

---
+++ bitop.cc
@@ -0,0 +1,22 @@
+/*
+@author gogdizzy
+@date   2015-08-20
+@desciption 收集到的一些位运算代码片段
+*/
+
+// 返回大于等于x的最小的2次幂，0返回0，负数返回0
+// 根据这个方法，可以很容易写出无符号版本和64bit版本
+int32_t nextpow2( int32_t x ) {
+	--x;
+	x |= ( x >> 1 );
+	x |= ( x >> 2 );
+	x |= ( x >> 4 );
+	x |= ( x >> 8 );
+	x |= ( x >> 16 );
+	return ++x;
+}
+
+// 解析出最低bit
+int32_t lowbit( int32_t x ) {
+	return x & -x;
+}
